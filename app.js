d28a3c2f4f367be86071b548c815704c56ee2012
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-10-01 11:22:43 -0700

    Uploading initial starter code

---
+++ app.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/** Express app for Lunchly. */
+
+const Express = require("express");
+const nunjucks = require("nunjucks");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const { NotFoundError } = require("./expressError");
+
+const app = new Express();
+
+// Parse body for urlencoded (non-JSON) data
+app.use(bodyParser.urlencoded({ extended: false }));
+
+nunjucks.configure("templates", {
+  autoescape: true,
+  express: app,
+});
+
+app.use(routes);
+
+
+/** 404 handler: matches unmatched routes; raises NotFoundError. */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+/** Error handler: logs stacktrace and returns JSON error message. */
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+  return res.status(status).json({ error: { message, status } });
+});
+
+
+module.exports = app;
